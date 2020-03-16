a2898aa4c6b6cd50c07e73636b580a07c0f073fa
sola <530830311@qq.com>
2020-03-16 10:27:31 +0800

    gjg

--- wasteland/urls.py
+++ wasteland/urls.py
@@ -19,38 +19,42 @@ import webaccount.views
 import serverconf.views
 import push.views
 import common.views
+import charge.views
 
 '''对原有做兼容修改'''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('common/', include('common.urls')),
-    path('server/', include('serverconf.urls')),
+    # path('common/', include('common.urls')),
+    # ('server/', include('serverconf.urls')),
     # path('weight/', include('serverweight.urls')),
-    path('charge/', include('charge.urls')),
-    path('push/', include('push.urls')),
-    path('account/', include('webaccount.urls')),
+    # path('charge/', include('charge.urls')),
+    # path('push/', include('push.urls')),
+    # path('account/', include('webaccount.urls')),
 
     # 兼容之前
     path('accountserver.php', webaccount.views.user_login),
     path('playerlogin.php', webaccount.views.enter_game),
-    path('bindaccount.php', webaccount.views.bind_account),
+    path('bindaccount.php', webaccount.views.bind_account),     #
     path('servercfg.php', serverconf.views.game_config),
     path('allserverdbcfg', serverconf.views.all_server_dbconfig),
-    path('xindong/onlines', webaccount.views.xindong_online),
-    path('fcm_broadcast.php', push.views.broadcast_push),
+    path('xindong/onlines', webaccount.views.xindong_online),   #
+    path('fcm_broadcast.php', push.views.broadcast_push),       #
+    path('fcm_register.php', push.views.register_push),     #
+    path('fcm_unregister.php', push.views.unregister_push),     #
     path('translate.php', common.views.translate),
     path('clientversion', common.views.get_client_version),
     path('saveclientver', common.views.set_client_version),
     path('gmserverlist.php', serverconf.views.get_game_list),
     path('gmupdateserver.php', serverconf.views.update_game_server),
     path('countryweight', common.views.save_country_weight),
+    path('xindong/pay', charge.views.xindong_pay),  #
+    path('chargeplayer', charge.views.charge_player),   #
 
     path('islockaccount.php', webaccount.views.is_account_lock),
     path('lockaccount.php', webaccount.views.lock_account),
     path('accountoflock.php', webaccount.views.get_lock_account),
-    path('islockip.php', webaccount.views.is_lockip),
-    path('ipoflock.php', webaccount.views.lockip_list),
-    path('lockip.php', webaccount.views.lock_ip),
-
+    path('islockip.php', webaccount.views.is_lockip),   #
+    path('ipoflock.php', webaccount.views.lockip_list),     #
+    path('lockip.php', webaccount.views.lock_ip),   #
 ]
